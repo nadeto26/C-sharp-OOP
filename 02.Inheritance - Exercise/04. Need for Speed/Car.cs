da59dbdf7e277bfcbad7dcec78283bfa33867a22
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-12 18:20:26 +0200

    Test

---
+++ 02.Inheritance - Exercise/04. Need for Speed/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultFuelComsumtion = 3;
+        // затова е правейт да не може да бъде достъпена от другите класове 
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption 
+            => DefaultFuelComsumtion;
+    }
+}
---
+++ 02.Inheritance - Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelComsumtion = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+        private int horsePower;
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        private double fuel;
+
+        public double Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        public  virtual double FuelConsumption 
+            => DefaultFuelComsumtion;
+
+        public virtual void Drive (double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+
+
+    }
+}
