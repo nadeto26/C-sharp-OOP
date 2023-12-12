73a7681783f1895f3b5948a6c563e4d545984721
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-12 18:20:02 +0200

    Test

---
+++ 02.Inheritance - Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Knight kn = new Knight("KNI", 2);
+            Console.WriteLine(kn);
+        }
+    }
+}
