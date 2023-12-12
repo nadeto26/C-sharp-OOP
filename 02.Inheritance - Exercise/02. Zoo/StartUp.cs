f3b59fa1186e6bc4b66281e1735757b166ff4e2f
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-12 18:19:28 +0200

    Test

---
+++ 02.Inheritance - Exercise/02. Zoo/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Gorilla goril = new Gorilla("Chon");
+            Snake snk = new Snake("sm");
+            Console.WriteLine(goril);
+        }
+    }
+}
