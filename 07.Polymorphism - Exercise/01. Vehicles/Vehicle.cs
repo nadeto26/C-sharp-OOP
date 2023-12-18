4a3e9d8ab7a37a84842ec9b509ed1bcf000f4006
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-18 16:07:49 +0200

    Test

---
+++ 07.Polymorphism - Exercise/01. Vehicles/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp163
+{
+    public abstract class Vehicle  
+    {
+        private double fuelQuantity;
+        private double consumprionPerKilometer;
+        private double tankCapacity;
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public double ConsumprionPerKilometer
+        {
+            get { return consumprionPerKilometer; }
+            set { consumprionPerKilometer = value; }
+        }
+
+        public double TankCapacity
+        {
+            get { return tankCapacity; }
+            set
+            { tankCapacity = value; }
+        }
+
+
+        public abstract void Drive(double distance);
+        public abstract void Refuel(double liters);
+
+    }
+}
