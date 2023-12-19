ad5568b0de9228bf3406b2d23a261750dda0d2fb
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-19 18:57:30 +0200

    Test

---
+++ 08.Exceptions and Error Handling - Lab/02. Enter Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _02._Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+
+            int end = 100;
+
+            int validNumbers = 10;
+
+            int[] numbers = new int[validNumbers];
+
+            for (int i = 0; i < validNumbers; i++)
+            {
+                try
+                {
+                    numbers[i] = ReadNumber(start, end);
+
+                    start = numbers[i];
+                }
+
+                catch (ArgumentOutOfRangeException argumentOutOfRangeException)
+                {
+                    Console.WriteLine(argumentOutOfRangeException.Message);
+
+                    i--;
+                }
+
+                catch (FormatException formatException)
+                {
+                    Console.WriteLine(formatException.Message);
+
+                    i--;
+                }
+            }
+
+            Console.Write(string.Join(", ", numbers));
+
+            static int ReadNumber(int start, int end)
+            {
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    if ((number <= start) || (number >= end))
+                    {
+                        throw new ArgumentOutOfRangeException(null, $"Your number is not in range {start} - {end}!");
+                    }
+                }
+
+                else
+                {
+                    throw new FormatException("Invalid Number!");
+                }
+
+                return number;
+            }
+        }
+    }
+}
