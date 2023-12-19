0164daeeca045e096233d26497bcf1f87bac26e0
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-19 18:56:42 +0200

    Test

---
+++ 08.Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(number), "Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(number));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
