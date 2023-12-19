385f1e535321ca69969ae0a69dbe678937375712
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-19 18:58:21 +0200

    Test

---
+++ 08.Exceptions and Error Handling - Lab/04. Sum of Integers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _04._Sum_of_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            int sum = 0;
+
+
+            foreach (string value in input)
+            {
+                try
+                {
+                    int number = int.Parse(value);
+                    sum += number;
+                }
+                catch (FormatException ex)
+                {
+
+                    Console.WriteLine($"The element '{value}' is in wrong format!");
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine($"The element '{value}' is out of range!");
+                }
+
+                finally
+                {
+                    Console.WriteLine($"Element '{value}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+
+
+
+        }
+    }
+}
