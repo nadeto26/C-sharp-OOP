d370cd200cb7259a07743dc5249dc1ad310ea35e
nadezhdakarapetrova@gmail.com <nadezhdakarapetrova@gmail.com>
2023-12-22 16:15:43 +0200

    Test

---
+++ Нова папка (2)/02. Business Logic/Core/Controller.cs
@@ -0,0 +1,68 @@
+﻿using RobotService.Core.Contracts;
+using RobotService.Models;
+using RobotService.Models.Contracts;
+using RobotService.Repositories;
+using RobotService.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Core
+{
+    public class Controller : IController
+    {
+        private SupplementRepository supplements;
+        private RobotRepository robots;
+        public Controller()
+        {
+            this.supplements = new SupplementRepository();
+            this.robots = new RobotRepository();
+        }
+
+        public string CreateRobot(string model, string typeName)
+        {
+            IRobot robot;
+            if (typeName ==nameof(DomesticAssistant))
+            {
+                robot = new DomesticAssistant(model);
+            }
+            else if (typeName == nameof(IndustrialAssistant))
+            {
+                robot = new IndustrialAssistant (model);
+            }
+            else
+            {
+                return string.Format(OutputMessages.RobotCannotBeCreated);
+            }
+            this robots.AddNew(robot);
+
+        }
+
+        public string CreateSupplement(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string PerformService(string serviceName, int intefaceStandard, int totalPowerNeeded)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RobotRecovery(string model, int minutes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpgradeRobot(string model, string supplementTypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
